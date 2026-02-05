29adaf3832dd51928ae83624fcfc6ff4335b9fc2
Ankur Debnath <ankurdebnath250@gmail.com>
2026-02-05 23:59:40 +0530

    shortcut

---
+++ Numbers/FibonacciNumber.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class FibonacciNumber {
+    public static int fibonacciNumber(int n){
+        if (n<=1){
+            return n;
+        }
+
+        int second_last = 0;
+        int last= 1;
+
+        for (int i = 2; i <= n ; i++) {
+            int current = second_last + last;
+            second_last = last;
+            last = current;
+
+        }
+
+        return last;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        System.out.println(fibonacciNumber(n));
+    }
+
+}
